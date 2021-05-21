b12d79e27228c1efaed5f0d8f923d68733ad7bea
maperezr.14 <maperezr.14@gmail.com>
2021-05-21 13:08:08 -0500

    Vistas reportes

---
+++ resources/views/exports/apartment-price.blade.php
@@ -0,0 +1,47 @@
+<table cellpadding="0" cellspacing="0" width="100%" style="font-family: Calibri;font-size: 11px;text-align: center;color: #000;" border="0">
+	<thead>
+	  <tr style="vertical-align: middle;height: 60px;">
+	    <th colspan="3" style="border: 2px solid;border-right: 0;text-align: left;padding-left: 30px;">Logo Efron</th>
+	    <th colspan="5" style="font-size: 14px;border: 2px solid;border-left: 0;text-align: right;padding-right: 30px;">Proyecto: Mirador</th>
+	  </tr>
+	  <tr>
+	  	<th colspan="8">&nbsp;</th>
+	  </tr>
+	  <tr style="height: 40px;">
+	    <th colspan="6" style="border: 2px solid;border-right: 0;text-align: center;padding-left: 30px;font-size: 14px;background: #dee2e6;">DEPARTAMENTOS</th>
+	  	<th colspan="2" style="border: 2px solid;text-align: center;padding-left: 30px;font-size: 14px;background: #dee2e6;">PRECIOS DE VENTA</th>
+	  </tr>
+	    <tr style="background-color: #dee2e6;vertical-align: middle;height: 20px;">
+		    <th style="border: 1px solid;border-top: 0;border-left: 2px solid;border-bottom: 2px solid;">DPTO</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">TIPO</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">#DORM</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">A. Techada (M2)</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">A. Libre (M2)</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">A. Total (M2)</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">CONSTRUCCIÓN</th>
+		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 2px solid;">ENTREGA</th>
+		  </tr>
+	</thead>
+	<tbody style="border: 2px solid;">
+	  <tr>
+	    <td style="border: 1px solid;border-top: 0;">101</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">101 A</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">1</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">48.82</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">3.13</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">51.95</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">VENDIDO</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">VENDIDO</td>
+	  </tr>
+	  <tr>
+	    <td style="border: 1px solid;border-top: 0;">1409</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">1409 A</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">2</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">86.74</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">0</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">86.74</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">$ 197769</td>
+	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">$ 211613</td>
+	  </tr>
+	</tbody>
+</table>
