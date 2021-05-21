3ec592c324e1dbc259c17812e25ab210d3702d9e
maperezr.14 <maperezr.14@gmail.com>
2021-05-21 18:40:13 -0500

    Color borders

--- resources/views/exports/parking-prices.blade.php
+++ resources/views/exports/parking-prices.blade.php
@@ -1,47 +1,47 @@
 <table cellpadding="0" cellspacing="0" width="100%" style="font-family: Calibri;font-size: 11px;text-align: center;color: #000;" border="0">
 	<thead>
 	  <tr style="vertical-align: middle;height: 60px;">
-	    <th colspan="3" style="border: 2px solid;border-right: 0;text-align: left;padding-left: 30px;">Logo Efron</th>
-	    <th colspan="5" style="font-size: 14px;border: 2px solid;border-left: 0;text-align: right;padding-right: 30px;">Proyecto: Mirador</th>
+	    <th colspan="3" style="border: 2px solid#999999;border-right: 0;text-align: left;padding-left: 30px;">Logo Efron</th>
+	    <th colspan="5" style="font-size: 14px;border: 2px solid#999999;border-left: 0;text-align: right;padding-right: 30px;">Proyecto: Mirador</th>
 	  </tr>
 	  <tr>
 	  	<th colspan="8">&nbsp;</th>
 	  </tr>
 	  <tr style="height: 40px;">
-	    <th colspan="7" style="border: 2px solid;border-right: 0;text-align: center;padding-left: 30px;font-size: 14px;background: #dee2e6;">ESTACIONAMIENTOS</th>
-	  	<th colspan="1" style="border: 2px solid;text-align: center;padding-left: 30px;font-size: 14px;background: #dee2e6;">PRECIOS DE VENTA</th>
+	    <th colspan="7" style="border: 2px solid#999999;border-right: 0;text-align: center;padding-left: 30px;font-size: 14px;background: #dee2e6;">ESTACIONAMIENTOS</th>
+	  	<th colspan="1" style="border: 2px solid#999999;text-align: center;padding-left: 30px;font-size: 14px;background: #dee2e6;">PRECIOS DE VENTA</th>
 	  </tr>
 	    <tr style="background-color: #dee2e6;vertical-align: middle;height: 20px;">
-		    <th style="border: 1px solid;border-top: 0;border-left: 2px solid;border-bottom: 2px solid;">PISO</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">ESTAC.</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">TIPO</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">A. Techada (M2)</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">A. Libre (M2)</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">A. Total (M2)</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 1px solid;">CLOSET</th>
-		    <th style="border-top: 0;border-bottom: 2px solid;border-right: 2px solid;">PRECIO</th>
+		    <th style="border: 1px solid #999999;border-top: 0;border-left: 2px solid#999999;border-bottom: 2px solid#999999;">PISO</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 1px solid #999999;">ESTAC.</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 1px solid #999999;">TIPO</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 1px solid #999999;">A. Techada (M2)</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 1px solid #999999;">A. Libre (M2)</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 1px solid #999999;">A. Total (M2)</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 1px solid #999999;">CLOSET</th>
+		    <th style="border-top: 0;border-bottom: 2px solid#999999;border-right: 2px solid#999999;">PRECIO</th>
 		  </tr>
 	</thead>
-	<tbody style="border: 2px solid;">
+	<tbody style="border: 2px solid#999999;">
 	  <tr>
-	    <td style="border: 1px solid;border-top: 0;">Piso 1</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">Faustino Sánchez Carrión 156</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">Doble</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">18.85</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">6.21</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">25.06</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">No</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">VENDIDO</td>
+	    <td style="border: 1px solid #999999;border-top: 0;">Piso 1</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">Faustino Sánchez Carrión 156</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">Doble</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">18.85</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">6.21</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">25.06</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">No</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">VENDIDO</td>
 	  </tr>
 	  <tr>
-	    <td style="border: 1px solid;border-top: 0;">Sotano 1</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">01</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">Simple</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">14.63</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">0</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">14.63</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">No</td>
-	    <td style="border-top: 0;border-bottom: 1px solid;border-right: 1px solid;">$ 50625</td>
+	    <td style="border: 1px solid #999999;border-top: 0;">Sotano 1</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">01</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">Simple</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">14.63</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">0</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">14.63</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">No</td>
+	    <td style="border-top: 0;border-bottom: 1px solid #999999;border-right: 1px solid #999999;">$ 50625</td>
 	  </tr>
 	</tbody>
 </table>
